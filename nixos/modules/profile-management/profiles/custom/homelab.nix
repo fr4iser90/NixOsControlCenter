c830c509f9ca91bd8a5af44f57cc64ae8e0276b1
fr4iser90 <pa.boe90@gmail.com>
2024-12-22 19:15:42 +0100

    added firefox for homelab for testing

--- nixos/modules/profile-management/profiles/custom/homelab.nix
+++ nixos/modules/profile-management/profiles/custom/homelab.nix
@@ -6,7 +6,7 @@
   # Basis-Konfiguration für Homelab
   services.openssh.enable = true;
   virtualisation.docker.enable = true;
-  
+  programs.firefox.enable = true;
   # Optional GUI
   services.xserver.enable = lib.mkDefault true;
 
